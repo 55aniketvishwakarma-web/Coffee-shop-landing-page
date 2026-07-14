adda6802f6cc842f6f0800ef438c08214fb59edf
Aniket Vishwakarma <55aniketvishwakarma@gmail.com>
2026-07-15 01:14:42 +0530

    Added hamburger menu

--- script.js
+++ script.js
@@ -14,3 +14,12 @@ topBtn.onclick = function () {
         behavior: "smooth"
     });
 };
+
+const menuToggle = document.getElementById("menu-toggle");
+const navMenu = document.getElementById("nav-menu");
+
+menuToggle.addEventListener("click", () => {
+    navMenu.classList.toggle("active");
+});
+
+console.log("JavaScript Loaded");
